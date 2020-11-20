81ee618c5177be8c61e2acc234fafc30a0688762
root <root@MehraSaeed.localdomain>
2020-11-20 19:37:50 +0330

    fix some bug on tools and install.sh and BruteSploit

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 #sysupdate
 apt update -y
 #install req
-apt install -y git medusa crunch cewl fcrackzip python3 python make tor openssl
+apt install -y gcc git medusa crunch cewl fcrackzip python3 python make tor openssl
 #make directory
 mkdir tools
 #install tor multiport
-https://github.com/samsesh/multitor.git
+git clone https://github.com/samsesh/multitor.git
 #install dirsearch
 cd ./tools
 git clone https://github.com/maurosoria/dirsearch.git
@@ -14,19 +14,19 @@ cd dirsearch
 pip3 install -r requirements.txt
 pip install -r requirements.txt
 pip3.exe install -r requirements.txt
-cd ./../
+cd ..
 # install cupp
 git clone https://github.com/Mebus/cupp.git
-cp -r cupp.cfg ./cupp/
 # install CeWL
 git clone https://github.com/digininja/CeWL.git
 # install ceunch
 git clone https://github.com/crunchsec/crunch.git
-cp -r crunch.sh ./crunch/
 cd crunch
-mv crunch.sh crunch
 make
 #run
 cd ./../../
+#cupp conf
+rm ./tools/cupp/cupp.cfg
+cp ./tools/cupp.cfg ./tools/cupp/
 chmod +x Brutesploit
 ./Brutesploit
