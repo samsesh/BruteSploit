1a5108decbd7bacdc111fe404a31cc7e9aacfacf
root <root@MehraSaeed.localdomain>
2020-11-20 23:16:36 +0330

    fix bug

--- install.sh
+++ install.sh
@@ -5,38 +5,61 @@ clear
 #install req
 apt install -y gcc git medusa crunch cewl fcrackzip python3 python make tor openssl
 clear
-#make directory
-cd tools
+#remove old version
+echo "remove old version file"
+rm -rf ./tools/multitor/
+rm -rf ./tools/dirsearch/
+rm -rf ./tools/cupp/
+rm -rf ./tools/CeWL/
+rm -rf ./tools/crunch/
+mkdir -p ./tools/multitor/
+mkdir -p ./tools/dirsearch/
+mkdir -p ./tools/cupp/
+mkdir -p ./tools/CeWL/
+mkdir -p ./tools/crnuch/
+sleep 5
+clear
+echo "download last version"
 #install tor multiport
-rm -rf multitor
-git clone https://github.com/samsesh/multitor.git
+git clone https://github.com/samsesh/multitor.git ./tools/multitor
+sleep 3
 clear
+echo "download last version"
 #install dirsearch
-rm -rf dirsearch
-git clone https://github.com/maurosoria/dirsearch.git
-cd dirsearch
-pip3 install -r requirements.txt
-pip3.exe install -r requirements.txt
-cd ..
+git clone https://github.com/maurosoria/dirsearch.git ./tools/dirsearch
+sleep 3
 clear
+echo "download last version"
 # install cupp
-rm -rf cupp/
-git clone https://github.com/Mebus/cupp.git
+git clone https://github.com/Mebus/cupp.git ./tools/cupp
+sleep 3
 clear
+echo "download last version"
 # install CeWL
-rm -rf CeWL
-git clone https://github.com/digininja/CeWL.git
+git clone https://github.com/digininja/CeWL.git ./tools/CeWL
+sleep 3
 clear
+echo "download last version"
 # install cerunch
-rm -rf cerunch/
-git clone https://github.com/crunchsec/crunch.git
-cd crunch
-make
-cd ..
+git clone https://github.com/crunchsec/crunch.git ./tools/crunch
+sleep 3
 clear
 #run
-cd ..
 echo "run config"
+sleep 3 
+clear
+echo "run config"\n
+echo "dirsearch config"
+cd ./tools/dirsearch
+pip3 install -r requirements.txt
+pip3.exe install -r requirements.txt
+cd ../..
+clear
+echo "run config"\n
+echo "config crunch"
+cd ./tools/crunch
+make
+cd ../..
 sleep 3
 #cupp conf
 rm ./tools/cupp/cupp.cfg
@@ -44,5 +67,6 @@ cp ./tools/cupp.cfg ./tools/cupp/
 chmod +x Brutesploit
 clear
 sleep 5
+echo "installing finish"\n
 echo "now run ./Brutesploit"
 sleep 5
